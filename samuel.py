260fd9f11d8f386259ea7f7c4421a913f23c5988
Tusemerirwesamuel <43774688+Tusemerirwesamuel@users.noreply.github.com>
2019-02-07 12:09:28 +0300

    Add files via upload

---
+++ samuel.py
@@ -0,0 +1,55 @@
+print("Hi am Samuel ")
+name = input("may i know you:")
+funny_fact = input("Do you do programming?")
+One_truth_about_you = input("Tell me what you are good at:")
+One_lie_about_you = input("Do you know python :")
+color = input("What are your favorite  languages?")
+
+print(f"What's up {name} Are you willing to help me coding as you do?" )
+answer = input("Yes or No: ")
+if (answer == "Yes"):
+    print(f"So we are now friends! {name} Thanks for pairing")
+if (answer == "No"):
+    	print(f"thats bad for programmers to not work in pairs, {name} No no no no that's not how programmers work!")
+if (answer == "bad"):
+	print(f"Sorry, {name} Lets hope it will get better!")   
+
+if (answer == "Good"):
+    print(f"Awesome! {name} Enjoy the rest of it")    
+
+if (answer == "cool"):
+    print(f"Awesome! {name} Enjoy the rest of it")        
+
+
+if (answer == "awesome"):
+    print(f"Awesome! {name} Enjoy the rest of it") 
+
+if (answer == "Fantastic"):
+    print(f"Awesome! {name} Enjoy the rest of it")       
+
+if (answer == "well"):
+    print(f"Awesome! {name} Enjoy the rest of it")    
+
+input("**Press Enter to Continue**: ")
+print("Great! Lets see if I still remember your favorite color! **Press Enter to continue**")
+
+print(f"I think its {color}")
+
+Color_input = input("Yes or No: ")
+if (Color_input == "Yes"):
+    print("Yeah!!! I got it right.....")
+
+if (Color_input == "yes"):
+    print("Yeah!!! I got it right.....")    
+if (Color_input == "No"):
+    print("Ooh no.... Guess again") 
+if (Color_input == "no"):
+    print("Ooh no.... Don't worry next time better Guess again") 
+
+input("**Press Enter to Continue**: ")
+print(f"It has been a pleasure chatting with you {name} and my favorite color too is {color}")  
+
+input("**Press Enter to Continue**: ")
+print("Thank you and goodbye!!")
+input("***Press CTRL + C to close the program***")     
+
